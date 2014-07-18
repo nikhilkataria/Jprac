ce3c98cdf05d830b3fbd53494d4885486dca26ed
nikhilkataria <kataria@buffalo.edu>
2014-07-17 22:21:58 -0700

    B
    B
    B
    B
    
    C

---
+++ src/newPackage/LinkListJava.java
@@ -0,0 +1,18 @@
+package newPackage;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class LinkListJava {
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		List<String> s = new ArrayList<String>();
+		s.add("sahud");
+		s.add("adsf");
+		System.out.println(s);
+	}
+
+}
